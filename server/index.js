daaec9a2cfc7d504c124a6bbe1abeee1f39b0c11
tjf91 <tudorjflorea@gmail.com>
2021-01-25 12:12:53 -0700

    added controller

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
+require('dotenv').config()
 const express = require('express')
+const app = express()
+const {SERVER_PORT}=process.env
+app.use(express.json())
+
+app.listen(SERVER_PORT, ()=>{
+    console.log(`Serving on port ${SERVER_PORT}`)
+})
