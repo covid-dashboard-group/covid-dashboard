09a47e67bc8d25525f4d44702d5f509beacbcc68
Mark Carey <markallencarey@gmail.com>
2021-01-26 12:41:19 -0700

    initial commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import { ReactComponent as DashboardLogo } from '../assets/DashboardLogo.svg'
-import { Navbar } from 'react-bootstrap'
+import { Navbar, Figure, Container } from 'react-bootstrap'
 
 const Header = (props) => {
 
   return (
     <div>
       <Navbar className='Header'>
-          <Navbar.Brand className='logo'>
-            <DashboardLogo />
-          </Navbar.Brand>
+        <Navbar.Brand md={2}>
+          <DashboardLogo
+            className='logo'
+          />
+          <Navbar.Text>
+            THE COVID-19 Dashboard
+          </Navbar.Text>
+        </Navbar.Brand>
+        <Container className='header-data-icons'>
+          <Container
+            className='header-icon-infections'
+          >
+            <p>TOTAL US INFECTIONS</p>
+          </Container>
+        </Container>
       </Navbar>
     </div>
   )
