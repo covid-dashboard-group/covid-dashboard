140cde000195efd2d529215bdfae5dd8002c9749
Mark Carey <markallencarey@gmail.com>
2021-01-28 16:24:29 -0700

    mapbox display fixed

--- src/Components/VisualsComponent/Map.js
+++ src/Components/VisualsComponent/Map.js
@@ -5,31 +5,31 @@ import dotenv from 'dotenv'
 import mapboxgl from 'mapbox-gl'
 import "mapbox-gl/dist/mapbox-gl.css";
 
-const { REACT_APP_MAPBOX_APIKEY} = process.env;
+const { REACT_APP_MAPBOX_APIKEY } = process.env;
 
 const Map = (props) => {
-  const mapContainer = useRef("");   
-    
-    mapboxgl.accessToken = REACT_APP_MAPBOX_APIKEY
+  const mapContainer = useRef("");
 
-    const styles = {
-        width: "800px",
-        height: "400px",
-        borderRadius: "10px"        
-    };
-    useEffect(()=>{
-        let map=new mapboxgl.Map({
-            container:mapContainer.current,
-            style:'mapbox://styles/mapbox/streets-v11',
-            center:[-111,33],
-            zoom:3
-        })
-      },[])
+  mapboxgl.accessToken = REACT_APP_MAPBOX_APIKEY
+
+  const styles = {
+    width: "100%",
+    height: "42vh",
+    borderRadius: "10px",
+  };
+  useEffect(() => {
+    let map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-111, 33],
+      zoom: 3
+    })
+  }, [])
   return (
-    <Container>
+    <Container className='Map'>
       <div ref={(el) => (mapContainer.current = el)} style={styles} />
     </Container>
-    
+
   )
 }
 
