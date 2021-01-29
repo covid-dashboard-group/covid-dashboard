075bb0dc56c96a8c4119f6a862d82b778a16e9f9
Mark Carey <markallencarey@gmail.com>
2021-01-29 16:48:37 -0700

    formatting articles

--- src/Components/MediaComponent/Article.js
+++ src/Components/MediaComponent/Article.js
@@ -24,19 +24,48 @@ const Article = (props) => {
 
   // useEffect(() => console.log("news", props.news))
 
+  function handleTitleLength() {
+    let shortTitle = []
+    let { title } = props.article
+    // console.log('title', title)
+    let titleArr = title.split('')
+    // console.log(titleArr)
+
+    if (titleArr.length > 45) {
+      for (let i = 0; i < 45; i++) {
+        shortTitle.push(titleArr[i])
+      }
+      shortTitle.push('.')
+      shortTitle.push('.')
+      shortTitle.push('.')
+      console.log('shortTitle', shortTitle)
+      let titleJoined = shortTitle.join('')
+      console.log(titleJoined)
+      return titleJoined
+    } else {
+      console.log(title)
+      return title
+    }
+  }
+
   return (
     <Card className='Article'>
-      <Card.Body>
-        <Card.Image
-          src={props.article.urlToImage}
-        />
+      <Card.Body className='article-body'>
         <Card.Title className='article-title'>
-          {props.article.title}
+          {handleTitleLength()}
         </Card.Title>
-        <Card.Subtitle className='mb-2 text-muted'>
+        <Card.Subtitle
+        class='text-muted'  className='article-subtitle'>
           {props.article.source.name}
         </Card.Subtitle>
       </Card.Body>
+      {props.article.urlToImage ? (
+        <Card.Img
+          src={props.article.urlToImage}
+          variant='side'
+          className='article-img'
+        />
+      ) : <div className='article-no-img'></div>}
     </Card>
   )
 }
